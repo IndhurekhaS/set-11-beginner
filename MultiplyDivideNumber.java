541456383f444c752d341d1b2545b43ff610a0b9
IndhurekhaS <42089681+IndhurekhaS@users.noreply.github.com>
2019-07-18 11:36:48 +0530

    Create MultiplyDivideNumber.java

---
+++ MultiplyDivideNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int c=sc.nextInt();
+       int sum=a*b;
+       int add=sum/c;
+       System.out.println(add);
+    }
+}
