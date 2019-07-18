cb3d1986d87dd822f77961ab668d709c0c9b51b4
IndhurekhaS <42089681+IndhurekhaS@users.noreply.github.com>
2019-07-18 10:18:38 +0530

    Create ConcatTwoNumber.java

---
+++ ConcatTwoNumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       int k=sc.nextInt();
+       String n1=String.valueOf(n);
+       String k1=String.valueOf(k);
+       System.out.println(n1+k1);
+    }
+}
